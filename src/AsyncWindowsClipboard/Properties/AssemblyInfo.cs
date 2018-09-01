60bd22cf5fcb68859c779f0918e6d79f5f6e3f15
undergroundwires <undergroundwires@users.noreply.github.com>
2018-09-02 01:14:03 +0200

    migrated to vs17 + more targets

--- src/AsyncWindowsClipboard/Properties/AssemblyInfo.cs
+++ src/AsyncWindowsClipboard/Properties/AssemblyInfo.cs
@@ -22,17 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b9b2ff2f-1ef9-4891-9a83-6c1d90ffb39b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
 
 [assembly: InternalsVisibleTo("AsyncWindowsClipboard.Tests")] //TODO: Add public key token
