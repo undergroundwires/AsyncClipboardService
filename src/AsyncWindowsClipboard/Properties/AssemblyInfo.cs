0fb389d1e914ae9cfaf93838954305abb8f6e583
undergroundwires <undergroundwires@users.noreply.github.com>
2019-12-31 21:05:46 +0100

    cleaned up

--- src/AsyncWindowsClipboard/Properties/AssemblyInfo.cs
+++ src/AsyncWindowsClipboard/Properties/AssemblyInfo.cs
@@ -1,26 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AsyncWindowsClipboard")]
-[assembly: AssemblyDescription("Thread-safe, asynchronous windows clipboard service with timeout strategy for .NET, C#.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/undergroundwires/")]
-[assembly: AssemblyProduct("AsyncClipboardService")]
-[assembly: AssemblyCopyright("Copyright ©  2016 - 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b9b2ff2f-1ef9-4891-9a83-6c1d90ffb39b")]
-
-
-[assembly: InternalsVisibleTo("AsyncWindowsClipboard.Tests")] //TODO: Add public key token
+[assembly: InternalsVisibleTo("AsyncWindowsClipboard.Tests")]
