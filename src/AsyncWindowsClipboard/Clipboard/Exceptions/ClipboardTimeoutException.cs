e21e49b83980eba3db9a759e5f03c0634224502c
undergroundwires <undergroundwires@users.noreply.github.com>
2020-01-10 03:54:18 +0100

    exceptions can be serialized + are simplified

--- src/AsyncWindowsClipboard/Clipboard/Exceptions/ClipboardTimeoutException.cs
+++ src/AsyncWindowsClipboard/Clipboard/Exceptions/ClipboardTimeoutException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace AsyncWindowsClipboard.Clipboard.Exceptions
 {
@@ -8,26 +9,24 @@ namespace AsyncWindowsClipboard.Clipboard.Exceptions
     ///     multiple <see cref="ClipboardWindowsApiException" />s.
     /// </summary>
     /// <seealso cref="ClipboardWindowsApiException" />
-    public sealed class ClipboardTimeoutException : Exception
+    [Serializable]
+    public sealed class ClipboardTimeoutException : AggregateException, ISerializable
     {
         /// <exception cref="ArgumentNullException"><paramref name="innerException" /> is null.</exception>
         /// <exception cref="ArgumentException"><paramref name="message" /> cannot be null or empty.</exception>
         public ClipboardTimeoutException(string message, ClipboardWindowsApiException innerException)
-            : base(GetExceptionMessageWithInnerPropertyReference(message, nameof(InnerException)))
+            : this(GetExceptionMessageWithInnerPropertyReference(message, nameof(InnerException)), new []{innerException})
         {
-            if (string.IsNullOrEmpty(message))
-                throw new ArgumentException("Value cannot be null or empty.", nameof(message));
-            InnerException = innerException ?? throw new ArgumentNullException(nameof(innerException));
+            if (innerException == null) throw new ArgumentNullException(nameof(innerException));
         }
 
         /// <exception cref="ArgumentNullException"><paramref name="innerExceptions" /> is null.</exception>
         /// <exception cref="ArgumentException"><paramref name="message" /> cannot be null or empty.</exception>
         public ClipboardTimeoutException(string message, IEnumerable<ClipboardWindowsApiException> innerExceptions)
-            : base(GetExceptionMessageWithInnerPropertyReference(message, nameof(InnerExceptions)))
+            : base(GetExceptionMessageWithInnerPropertyReference(message, nameof(InnerExceptions)), innerExceptions)
         {
             if (string.IsNullOrEmpty(message))
                 throw new ArgumentException("Value cannot be null or empty.", nameof(message));
-            InnerExceptions = innerExceptions ?? throw new ArgumentNullException(nameof(innerExceptions));
         }
 
         /// <exception cref="ArgumentException"><paramref name="message" /> cannot be null or empty.</exception>
@@ -37,15 +36,14 @@ namespace AsyncWindowsClipboard.Clipboard.Exceptions
                 throw new ArgumentException("Value cannot be null or empty.", nameof(message));
         }
 
-        /// <summary>
-        ///     The inner exception that caused this exception.
-        /// </summary>
-        public new ClipboardWindowsApiException InnerException { get; }
-
-        /// <summary>
-        ///     List of innerexceptions that caused this exception.
-        /// </summary>
-        public IEnumerable<ClipboardWindowsApiException> InnerExceptions { get; }
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="info"/> parameter is <c>null</c>.</exception>
+        /// <exception cref="SerializationException">The class name is <c>null</c> or <see cref="Exception.HResult"/> is zero (0).</exception>
+        private ClipboardTimeoutException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
 
         private static string GetExceptionMessageWithInnerPropertyReference(string message, string propertyName)
             => $"{message}{Environment.NewLine}Check the inner exceptions ({propertyName} property) for more details.";
--- src/AsyncWindowsClipboard/Clipboard/Exceptions/ClipboardWindowsApiException.cs
+++ src/AsyncWindowsClipboard/Clipboard/Exceptions/ClipboardWindowsApiException.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace AsyncWindowsClipboard.Clipboard.Exceptions
 {
@@ -7,6 +8,7 @@ namespace AsyncWindowsClipboard.Clipboard.Exceptions
     ///     This type of exception is thrown if there has been any errors during communication with windows api.
     /// </summary>
     /// <seealso cref="Win32Exception" />
+    [Serializable]
     public sealed class ClipboardWindowsApiException : Win32Exception
     {
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="error" /> is not unsigned</exception>
@@ -20,6 +22,15 @@ namespace AsyncWindowsClipboard.Clipboard.Exceptions
         {
             if (string.IsNullOrEmpty(message))
                 throw new ArgumentException("Value cannot be null or empty.", nameof(message));
+        }
+
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="info"/> parameter is <c>null</c>.</exception>
+        /// <exception cref="SerializationException">The class name is <c>null</c> or <see cref="Exception.HResult"/> is zero (0).</exception>
+        public ClipboardWindowsApiException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
         }
     }
 }
